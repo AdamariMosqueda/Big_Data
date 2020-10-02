27942e820a644f938cadef940a7089c2efb11ca4
AdamariMosqueda <adamari.mosqueda16@tectijuana.edu.mx>
2020-10-01 19:33:51 -0700

    Practice 1 part 1

---
+++ Practices/Practice 1.scala
@@ -0,0 +1,18 @@
+//1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
+
+
+//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+
+//3. Given the variable bird = "tweet", use interpolation of string to print "I'm writing a tweet"
+val bird = "tweet"
+val tweet = s"I'm writing a $bird"
+
+//4. Given the variable message = "Hello Luke, I am your father!" use slilce to extract the sequence "Luke"
+val message = "Hello Luke, I am your father!"
+message slice  (6,10)
+
+//5. What's the difference between value and variable in scala?
+
+//6. Given the tuple (2,4,5,1,2,3,3.1416,23) returns 3.1416 
+val MyTuple = (2,4,5,1,2,3,3.1416,23)
+MyTuple._7
