c95edefa76f87479e66f3994622065627409825f
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2020-10-12 16:58:58 -0700

    Practice 2 finished and small changes in readme

--- Practices/Practice 2.scala
+++ Practices/Practice 2.scala
@@ -1,8 +1,24 @@
 //Practice 2
-// 1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
-// 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
-// 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
-// 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
+
+// 1. Create a list called "list" with the elements "red", "white", "black"
+val list = List("red","white","black")
+
+// 2. Add 5 more items to "list" "green", "yellow", "blue", "orange", "pearl"
+val list= collection.mutable.Set("red","white","black")
+list+="green"
+list+= "yellow"
+list+="blue"
+list+= "orange"
+list+="pearl"
+
+// 3. Bring the items from "list" "green", "yellow", "blue"
+for (lists <- list) { 
+    if(lists =="green" || lists =="yellow" || lists=="blue")
+    println(lists)
+    }
+
+// 4. Create an Array of numbers in the range 1-1000 in steps of 5 by 5
+val arrNum=Array.range(1,1000,5)
 
 // 5.  What are the unique elements of the list list(1,3,3,4,6,7,3,7)  use conversion to sets
 val list = List(1,3,3,4,6,7,3,7)
--- README.md
+++ README.md
@@ -55,6 +55,30 @@ Al definir MyTuple._7 seleccionamos el elemento número 7 de aquella tupla
 
 ## Practice 2 
 En esta practica hacemos uso de listas, arreglos y mapas 
+> 1.- Create a list called "list" with the elements "red", "white", "black"
+```
+val list = List("red","white","black")
+```
+>2.- Add 5 more items to "list" "green", "yellow", "blue", "orange", "pearl"
+```
+val list= collection.mutable.Set("red","white","black")
+list+="green"
+list+= "yellow"
+list+="blue"
+list+= "orange"
+list+="pearl"
+```
+>3.- Bring the items from "list" "green", "yellow", "blue"
+```
+for (lists <- list) { 
+    if(lists =="green" || lists =="yellow" || lists=="blue")
+    println(lists)
+    }
+```
+4.-  Create an Array of numbers in the range 1-1000 in steps of 5 by 5
+```
+val arrNum=Array.range(1,1000,5)
+```
 > 5.- What are the unique elements of the list list(1,3,3,4,6,7,3,7)  use conversion to sets
 ```
 val list = List(1,3,3,4,6,7,3,7)
