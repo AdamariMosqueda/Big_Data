65c715ab2e7c77cc50c3170b9943b3d0b1501be5
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2020-10-23 11:15:48 -0700

    New dataframe HV Ratio

--- Evaluation/Exam U1.scala
+++ Evaluation/Exam U1.scala
@@ -28,6 +28,7 @@ df.describe().show()
 //7. Crea un nuevo dataframe con una columna nueva llamada “HV Ratio” que es la
 //relación entre el precio de la columna “High” frente a la columna “Volume” de
 //acciones negociadas por un día. (Hint: Es una operación de columnas).
+val df2 = df.withColumn("HV Ratio", df("High")+df("Volume"))
 //8. ¿Qué día tuvo el pico mas alto en la columna “Close”?
 //9. Escribe con tus propias palabras en un comentario de tu codigo. ¿Cuál es el
 //significado de la columna Cerrar “Close”?
