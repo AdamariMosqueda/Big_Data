2b50a4991ec3dbce13f5c93ef5c70e89f2a3cd37
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2020-12-16 10:01:34 -0800

    traslate file evaluative practice unit 3

--- U3/Evaluation/Evaluative_PracticeU3.scala
+++ U3/Evaluation/Evaluative_PracticeU3.scala
@@ -1,36 +1,35 @@
 //Evaluative Practice Unit 3
 // K means
-//1. Importar una simple sesión Spark.
+//1. Import a simple Spark session.
 import org.apache.spark.sql.SparkSession
-//2. Utilice las lineas de código para minimizar errores
+//2. Use lines of code to minimize errors
 import org.apache.log4j._
 Logger.getLogger("org").setLevel(Level.ERROR)
 
-//3. Cree una instancia de la sesión Spark
+//3. Create an instance of the Spark session
 val spark = SparkSession.builder().getOrCreate()
-//4. Importar la librería de Kmeans para el algoritmo de agrupamiento.
+//4. Import the Kmeans library for the clustering algorithm
 import org.apache.spark.ml.clustering.KMeans
 
-//5. Carga el dataset de Wholesale Customers Data
+//5. Loads the Wholesale Customers Data dataset
 val dataset = spark.read.option("header","true").option("inferSchema","true").csv("Wholesale customers data.csv")
-//6. Seleccione las siguientes columnas: Fresh, Milk, Grocery, Frozen, Detergents_Paper, Delicassen y llamar a este conjunto feature_data
+//6. Select the following columns: Fresh, Milk, Grocery, Frozen, Detergents_Paper, Delicassen and call this set feature_data
 val feature_data = dataset.select("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")
 
-//7. Importar Vector Assembler y Vector
+//7. Import Vector Assembler and Vector
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Vectors
-//8. Crea un nuevo objeto Vector Assembler para las columnas de caracteristicas como un conjunto de entrada, recordando que no hay etiquetas
+//8. Create a new Vector Assembler object for the feature columns as an input set, remembering that there are no labels
 val VectorA = new VectorAssembler().setInputCols (Array ("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")). setOutputCol ("features")
 
-//9. Utilice el objeto assembler para transformar feature_data
+//9. Use the assembler object to transform feature_data
 val output= VectorA.transform(feature_data)
-//10. Crear un modelo Kmeans con K=3
+//10. Create a Kmeans model with K = 3
 val kmeans = new KMeans().setK(3).setSeed(1L)
 val model = kmeans.fit(output)
-//11. Evalúe  los grupos utilizando Within Set Sum of Squared Errors WSSSE e imprima los centroides.
+//11. Evaluate groups using Within Set Sum of Squared Errors WSSSE and print centroids.
 val WSSE = model.computeCost(output)
 println(s"Within set sum of Squared Errors = $WSSE")
 
-//centroides
 println("Cluster Centers: ")
 model.clusterCenters.foreach(println)
