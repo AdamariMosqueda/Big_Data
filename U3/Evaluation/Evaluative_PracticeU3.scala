d445523ea61809ec951b4e4abe89123260128373
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2020-12-15 19:51:57 -0800

    Add import library vector and vassembler

--- U3/Evaluation/Evaluative_PracticeU3.scala
+++ U3/Evaluation/Evaluative_PracticeU3.scala
@@ -12,12 +12,13 @@ val spark = SparkSession.builder().getOrCreate()
 import org.apache.spark.ml.clustering.KMeans
 
 //5. Carga el dataset de Wholesale Customers Data
-val dataset = spark.read.option("header","true").option("inferSchema","true").csv("C:/Wholesale customers data.csv")
+val dataset = spark.read.option("header","true").option("inferSchema","true").csv("Wholesale customers data.csv")
 //6. Seleccione las siguientes columnas: Fresh, Milk, Grocery, Frozen, Detergents_Paper, Delicassen y llamar a este conjunto feature_data
 val feature_data = dataset.select("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")
 
 //7. Importar Vector Assembler y Vector
-
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 //8. Crea un nuevo objeto Vector Assembler para las columnas de caracteristicas como un conjunto de entrada, recordando que no hay etiquetas
 
 //9. Utilice el objeto assembler para transformar feature_data
