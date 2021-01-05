4e48e6b29d949db5914072f448709478a92294da
Nayeli Gutierrez <yuridia.gutierrez16@tectijuana.edu.mx>
2021-01-05 14:48:26 -0800

    Update file

--- Project/SVM.scala
+++ Project/SVM.scala
@@ -5,16 +5,14 @@ import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Vectors
 
-val data  = spark.read.option("header","true").option("delimiter", ";").format("csv").load("C:/bank-full.csv")
+val data  = spark.read.option("header","true").option("delimiter", ";").format("csv").load("bank-full.csv")
 //"duration", "contact", "month", "day", "default", "pdays" no nos sirven y hacemos un select de las que vamos a usar
 val data2 = data.select("age", "job", "marital", "education", "balance", "housing", "loan","campaign", "previous", "poutcome", "y")
 data2.show()
 
 //Son varias columnas con valores string por lo que hacemos un mapeo 
 val featureCol = data2.columns
-val indexers = featureCol.map { colName =>
-  new StringIndexer().setInputCol(colName).setOutputCol(colName + "Index")
-}
+val indexers = featureCol.map { colName =>new StringIndexer().setInputCol(colName).setOutputCol(colName + "Index")}
 //Usamos pipelane para hacer la transformación
 val pipeline = new Pipeline().setStages(indexers)      
 val newDF = pipeline.fit(data2).transform(data2)
